1258bb9eb535fa7a1c9f8278d81a3837e7e2ab27
a.gabitova <a.gabitova@DESKTOP-I7KTRRV>
2023-02-09 16:27:39 +0300

    Добавьте файлы проекта.

---
+++ TestBD/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TestBD.Data;
+using TestBD.Models;
+
+namespace TestBD.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TestBD.Data.SchoolContext _context;
+
+        public CreateModel(TestBD.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Student.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+
+        //todo: 1. Add genre type. 2. Add lazy load. 3. Return type MovieDto (Id, Title, GenreName, Price, Date)
+        // https://learn.microsoft.com/ru-ru/aspnet/core/tutorials/razor-pages/sql?view=aspnetcore-7.0&tabs=visual-studio - работа с базой данных
+    }
+}
---
+++ TestBD/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TestBD.Data;
+using TestBD.Models;
+
+namespace TestBD.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TestBD.Data.SchoolContext _context;
+
+        public DetailsModel(TestBD.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+      public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Student == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.Include(s => s.Enrollments).ThenInclude(e => e.Course).AsNoTracking().FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
